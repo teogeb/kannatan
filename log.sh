1d0067c9565ccc4a33b3cb87a9802296ed10bcc5
Teo Gebhard <teo@gebhard.fi>
2025-03-06 14:40:15 +0200

    rm beta-2024

--- log.sh
+++
@@ -1,1 +0,0 @@
-aws logs tail --follow '/aws/lambda/kannatanfi-prod' --profile $1 --region eu-west-1 --color off --format short
--- tools/profile-generator/profiles.sh
+++
@@ -1,2 +0,0 @@
-cd output
-ls -1 | grep greyscale | sed 's/-greyscale.png//' | awk '{print $0 ".json"}' | xargs cat | jq -s '[.[]]' | jq 'map({statement: .statement | gsub("\"";""), reason, picture: (.party+"-"+(.created|tostring)+"-greyscale.png"), party: .party})'
