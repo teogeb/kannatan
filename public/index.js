1293426d735476b0177b08b4170d2809131c0922
Teo Gebhard <teo@gebhard.fi>
2025-03-11 03:18:10 +0200

    Persist party profiles (#27)
    The randomly assigned party-avatar mapping is stored in `localStorage`
    for one day.

--- public/index.js
+++ public/index.js
@@ -1,14 +1,34 @@
+const getLocalStorageJSON = (key, maxAge) => {
+    const value = localStorage.getItem(key)
+    if (value !== null) {
+        const parsed = JSON.parse(value)
+        if (parsed.timestamp >= Date.now() - maxAge) {
+            return parsed.data
+        }
+    }
+    return undefined
+}
+
+const setLocalStorageJSON = (key, value) => {
+    localStorage.setItem(key, JSON.stringify({ data: value, timestamp: Date.now() }))
+}
+
 const initPage = () => {
 
     const PARTY_IDS = ['kd', 'kesk', 'kok', 'ps', 'rkp', 'sdp', 'vas', 'vihr']
     const PROFILE_IDS = [0, 1, 2, 3, 4, 5, 6, 9, 11, 12, 13, 14, 16, 17, 18, 19, 20, 21, 22, 23, 24]
+    const MAX_PARTY_PROFILE_AGE = 86400000
 
-    const partyIds = _.shuffle(PARTY_IDS)
-    const profileIds = _.sampleSize(PROFILE_IDS, partyIds.length)
-    const partyProfiles = partyIds.map((partyId, i) => ({
-        partyId,
-        profileId: profileIds[i]
-    }))
+    let partyProfiles = getLocalStorageJSON('partyProfiles', MAX_PARTY_PROFILE_AGE)
+    if (partyProfiles === undefined) {
+        const partyIds = _.shuffle(PARTY_IDS)
+        const profileIds = _.sampleSize(PROFILE_IDS, partyIds.length)
+        partyProfiles = partyIds.map((partyId, i) => ({
+            partyId,
+            profileId: profileIds[i]
+        }))
+        setLocalStorageJSON('partyProfiles', partyProfiles)
+    }
 
     const partySelect = document.getElementsByClassName('partySelect')[0]
 
