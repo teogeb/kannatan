f727e470ff24edcf67bc84c2dcfc4964bd64daf8
Teo Gebhard <teo@gebhard.fi>
2025-03-14 14:03:19 +0200

    parenthesis

--- public/utils.js
+++ public/utils.js
@@ -15,7 +15,7 @@ const isMobile = () => {
 
 const initSubmitButtonStateController = (button, input) => {
     const updateState = () => {
-        button.disabled = input.value.trim() === ''
+        button.disabled = (input.value.trim() === '')
     }
     input.addEventListener('input', updateState)
     updateState()
