65576ac7bf01ea5b33b654274a6ff70181f9216c
Teo Gebhard <teo@gebhard.fi>
2025-03-17 23:15:00 +0200

    https configuration

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,16 @@ import 'dotenv/config'
 
 import { OpenAI as OpenAILlama } from '@llamaindex/openai'
 import express from 'express'
+import fs from 'fs'
+import http from 'http'
+import https from 'https'
 import { Settings } from 'llamaindex'
 import path from 'path'
+import { createChatResponse, deleteConversation } from './chat'
 import { sendMessageToTelegramAdminGroup } from './telegramBot'
 import { listDirectoryPaths, log } from './utils'
-import { createChatResponse, deleteConversation } from './chat'
 
 const app = express()
-const PORT = 8080
 
 Settings.llm = new OpenAILlama({
     model: 'gpt-4o-mini',
@@ -18,16 +20,9 @@ Settings.llm = new OpenAILlama({
     apiKey: process.env.OPENAI_API_KEY
 })
 
-// this is needed to get client IP address as deployment is behind a proxy (the AWS Application Load Balancer)
-app.set('trust proxy', true)
-
 app.use(express.json())
 app.use((_req, res, next) => {
     res.set('Accept-CH', 'Sec-CH-UA, Sec-CH-UA-Mobile, Sec-CH-UA-Platform')
-    // TODO not needed in production:
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
     next()
 })
 
@@ -76,11 +71,23 @@ app.post('/api/feedback', async (req, res) => {
     })
 })
 
-app.get('/healthcheck', (_req, res) => {
-    log('Healthcheck')
-    res.send('OK')
-})
-
-app.listen(PORT, () => {
-    log(`Server is running on http://localhost:${PORT}`)
-})
+if (process.env.HTTPS_DOMAIN !== undefined) {
+    const domain = process.env.HTTPS_DOMAIN
+    https.createServer({
+        key: fs.readFileSync(process.env.HTTPS_KEY_PATH as string),
+        cert: fs.readFileSync(process.env.HTTPS_CERT_PATH as string)
+    }, app).listen(443, () => {
+        console.log('HTTPS server running on port 443')
+    })
+    http.createServer((_req: http.IncomingMessage, res: http.ServerResponse) => {
+        res.writeHead(301, { "Location": `https://${domain}` });
+        res.end();
+    }).listen(80, () => {
+        console.log('HTTP server running on port 80 (Redirecting to HTTPS)')
+    })
+} else {
+    const PORT = 8080
+    app.listen(PORT, () => {
+        log(`Server is running on http://localhost:${PORT}`)
+    })
+}
