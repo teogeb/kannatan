c3b2fb9b959add51d5050d75eb572c5bb7aa7323
Teo Gebhard <teo@gebhard.fi>
2025-04-06 17:57:50 +0300

    front page: carousel UI

--- public/index.js
+++ public/index.js
@@ -1,35 +1,80 @@
 const initPage = () => {
 
-    const PARTY_IDS = ['kd', 'kesk', 'kok', 'lib', 'nyt', 'ps', 'rkp', 'sdp', 'vas', 'vihr']
-    const PROFILE_IDS = [..._.repeat('m', PARTY_IDS.length / 2), ..._.repeat('f', PARTY_IDS.length / 2)]
-    const MAX_PARTY_PROFILE_AGE = 86400000
-
-    let partyProfiles = getLocalStorageJSON('partyProfiles', MAX_PARTY_PROFILE_AGE)
-    if (partyProfiles === undefined) {
-        const partyIds = _.shuffle(PARTY_IDS)
-        const profileIds = _.shuffle(PROFILE_IDS)
-        partyProfiles = partyIds.map((partyId, i) => ({
-            partyId,
-            profileId: profileIds[i]
-        }))
-        setLocalStorageJSON('partyProfiles', partyProfiles)
+    const PARTIES = [
+        { id: 'kesk', name: 'Keskusta' },
+        { id: 'kok', name: 'Kokoomus' },
+        { id: 'kd', name: 'Kristillisdemokraatit' },
+        { id: 'lib', name: 'Liberaalipuolue' },
+        { id: 'nyt', name: 'Liike Nyt' },
+        { id: 'ps', name: 'Perussuomalaiset' },
+        { id: 'rkp', name: 'RKP' },
+        { id: 'sdp', name: 'SDP' },
+        { id: 'vas', name: 'Vasemmistoliitto' },
+        { id: 'vihr', name: 'Vihreät'  }
+    ]
+    
+    const MAX_PARTY_GENDER_AGE = 86400000
+    let partyGenders = getLocalStorageJSON('partyGenders', MAX_PARTY_GENDER_AGE)
+    if (partyGenders === undefined) {
+        partyGenders = [..._.repeat('m', PARTIES.length / 2), ..._.repeat('f', PARTIES.length / 2)]
+        setLocalStorageJSON('partyGenders', partyGenders)
     }
 
-    const partySelect = document.getElementsByClassName('partySelect')[0]
-
-    for (const partyProfile of partyProfiles) {
-        const element = document.createElement('div')
-        element.classList.add('party')
-        element.innerHTML = `
-            <a href="/chat?partyId=${partyProfile.partyId}&profileId=${partyProfile.profileId}">
-                <div class="img-container">
-                    <img src="/images/avatars/${partyProfile.partyId}-${partyProfile.profileId}.png" class="party-character">
-                    <img src="/images/logos/${partyProfile.partyId}.png" class="party-logo">
+    const swiperWrapper = document.querySelector('.swiper-wrapper')
+    PARTIES.forEach((party, i) => {
+        const slide = document.createElement('div')
+        slide.className = 'swiper-slide'
+        slide.innerHTML = `
+            <div class="party-item" data-party="${party.id}" class="no-select">
+                <div class="avatar-and-logo">
+                    <img class="avatar" src="/images/avatars/${party.id}-${partyGenders[i]}.png">
+                    <img class="party-logo" src="/images/logos/${party.id}.png">
                 </div>
-            </a>
+                <div class="party-name">${party.name}</div>
+            </div>
         `
-        partySelect.appendChild(element)
-    }
+        swiperWrapper.appendChild(slide)
+    })
+
+    const initialSlide = _.random(0, PARTIES.length - 1, false)
+    new Swiper('.swiper', {
+        loop: true,
+        effect: 'slide',
+        slidesPerView: 1,
+        spaceBetween: 100,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+            pauseOnMouseEnter: true
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+        },
+        keyboard: {
+            enabled: true
+        },
+        initialSlide,
+        on: {
+            init: function () {
+                document.querySelector('.swiper-button-next')?.classList.add('no-select')
+                document.querySelector('.swiper-button-prev')?.classList.add('no-select')
+            },
+            click: function (_swiper, event) {
+                const partyItem = event.target.closest('.party-item')
+                if (partyItem) {
+                    const partyId = partyItem.dataset.party
+                    const partyIndex = _.findIndex(PARTIES, (p) => p.id ===partyId)
+                    const gender = partyGenders[partyIndex]
+                    window.location.href = `/chat?partyId=${partyId}&profileId=${gender}`
+                }
+            }
+        }
+    })
 }
 
 document.addEventListener('DOMContentLoaded', () => initPage())
